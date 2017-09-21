113935ca896e3e65839fa712d6b58877c3f46de8
leeyii <lijie_666666@126.com>
2017-09-21 15:51:10 +0800

    创建OC微博Demo:Weibo-OC

---
+++ Weibo-OC/Weibo-OC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Weibo-OC
+//
+//  Created by leeyii on 2017/9/21.
+//  Copyright © 2017年 leeyii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
